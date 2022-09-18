6eef6b182d7f86a3078c6f98a8fcbb0a33211602
limmatfun <110715452+limmatfun@users.noreply.github.com>
2022-09-18 11:04:17 +0200

    New version with docker

--- breach/src/app/__init__.py
+++ breach/src/app/__init__.py
@@ -9,10 +9,14 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = uuid4().hex
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config["REDIS_URL"] = "redis://redis:6379"
 Session(app)
+
 redis_client = FlaskRedis(app)
 game_manager = GameManager(redis_client)
 
-# manage_session needs to be set to False because of
-# https://blog.miguelgrinberg.com/post/flask-socketio-and-the-user-session
-socketio = SocketIO(app, manage_session=False, logger=True, engineio_logger=True)
+# manage_session is by default true and that's ok
+# because we no longer change the session inside
+# a socket request
+# TODO: remove cors_allowed_origins
+socketio = SocketIO(app, manage_session=True, logger=True, engineio_logger=True, cors_allowed_origins="*")
